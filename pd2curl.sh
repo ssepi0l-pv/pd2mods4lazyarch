1ff2f5749faf869fcadb5644ade794652ab15ea0
ssepi0l-pv <64175868+ssepi0l-pv@users.noreply.github.com>
2020-07-02 22:06:18 -0400

    Update pd2curl.sh
    adding instructions for libcurl version

--- pd2curl.sh
+++ pd2curl.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-
+# so if you dont have libcurl4.6.0, dont worry. you just have to change the version in the line 5 and 8.
+# thats it, i think :)
 sudo cp /usr/lib/libcurl.so.4.6.0 /home/$USER/.local/share/Steam/ubuntu12_32/steam-runtime/pinned_libs_64/
 cd /home/$USER/.local/share/Steam/ubuntu12_32/steam-runtime/pinned_libs_64/
 sudo mv libcurl.so.4 libcurl.so.4.bkp
